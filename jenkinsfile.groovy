c30cb46094832a5c4a229e7229a7fc66edfb72e6
saurabh agrawal <saurabhagrawal2009@gmail.com>
2018-12-20 23:37:15 -0800

    checkmarx scan

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -7,5 +7,7 @@ node{
         sh "mvn clean install"
         }
     }
-    stage("deploy"){
+    stage("Checkmarx scan"){
+        step([$class: 'CxScanBuilder', comment: '', credentialsId: '', excludeFolders: '', excludeOpenSourceFolders: '', exclusionsSetting: 'global', failBuildOnNewResults: false, failBuildOnNewSeverity: 'HIGH', filterPattern: '', fullScanCycle: 10, groupId: 'Provide Checkmarx server credentials to see teams list', includeOpenSourceFolders: '', osaArchiveIncludePatterns: '*.zip, *.war, *.ear, *.tgz', osaInstallBeforeScan: false, password: '{AQAAABAAAAAQGi3BvhfECvL+QQuJ5NfgaEHoQP97hOH7RYF8rJx83hI=}', preset: 'Provide Checkmarx server credentials to see presets list', projectName: 'awscodedeploy', sastEnabled: true, serverUrl: '', sourceEncoding: 'Provide Checkmarx server credentials to see source encodings list', username: '', vulnerabilityThresholdResult: 'FAILURE', waitForResultsEnabled: true])
+
     }
